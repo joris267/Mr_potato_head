bf56b7520674993148faf33d87a118e2dc210767
joris_schefold <jorisschefold@gmail.com>
2015-04-10 15:56:31 +0200

    A working Mr potato head

--- app/src/main/java/com/example/joris/mrpotatohead/main_screen.java
+++ app/src/main/java/com/example/joris/mrpotatohead/main_screen.java
@@ -1,10 +1,15 @@
 package com.example.joris.mrpotatohead;
 
+import android.inputmethodservice.Keyboard;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 
@@ -14,6 +19,43 @@ public class main_screen extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+//        Code below doesn't work since checkbox.isSelected() returns false even when the checkbox is selected.
+//        I have an less elegant solution now by adding "android:configChanges="orientation|screenSize" to the manifest
+//        The code below is kept since I plan to try to fix this monday.
+////        Below code is to ensure that the app behaves properly on turning the screen.
+////        The problem was that it would keep the boxes checked but make all the accessories invisible.
+//        TableLayout container = (TableLayout)findViewById(R.id.button_container);
+//        int num_rows = container.getChildCount();
+//        int row_number;
+//
+////        First we loop over all the rows.
+//        for (row_number = 0; row_number < num_rows; row_number++){
+//            TableRow row = (TableRow)container.getChildAt(row_number);
+//
+//            int checkbox_cout = row.getChildCount();
+//            int checkbox_number;
+//
+////            Then we loop over every box in the row.
+//            for (checkbox_number = 0; checkbox_number < checkbox_cout; checkbox_number++) {
+//                CheckBox checkbox = (CheckBox) row.getChildAt(checkbox_number);
+//
+////                Then we get a link to the target image and check weather the box is checked.
+//                String target_img = checkbox.getTag().toString();
+//                boolean checked = checkbox.isSelected();
+//                System.out.println("HEEEEEEEEEERE  " + checked + "  " + target_img );
+//
+////                Lastly we get the imageview and set it to the correct visibility.
+//                int drawable_resource_id = this.getResources().getIdentifier(target_img, "id", this.getPackageName());
+//                ImageView image = (ImageView) findViewById(drawable_resource_id);
+//                if (checked) {
+//                    image.setVisibility(View.VISIBLE);
+//                } else {
+//                    image.setVisibility(View.GONE);
+//                }
+//            }
+//        }
+
     }
 
 
@@ -39,9 +81,21 @@ public class main_screen extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void onClick(View view) {
+        boolean checked = ((CheckBox) view).isChecked();
 
-    public void onClick(View v) {
-        int a = 1;
+//        get name of target image and convert it to id
+        String target_img = view.getTag().toString();
+        int drawable_resource_id = this.getResources().getIdentifier(target_img, "id", this.getPackageName());
+
+//        Get target imageView and use it to set visibility
+        ImageView image = (ImageView) findViewById(drawable_resource_id);
+        if (checked) {
+            image.setVisibility(view.VISIBLE);
+        }else{
+            image.setVisibility(view.GONE);
+        }
+
     }
 
 }
